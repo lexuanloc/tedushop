272a78b24d27b8b36e363247341c205b4c9e8ab9
lexuanloc <lexuanloc@outlook.com>
2020-02-13 11:05:15 +0700

    Add ASP.NET Identity

--- TeduShop.Data/Migrations/Configuration.cs
+++ TeduShop.Data/Migrations/Configuration.cs
@@ -1,9 +1,12 @@
 ﻿namespace TeduShop.Data.Migrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using TeduShop.Model.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<TeduShop.Data.TeduShopDbContext>
     {
@@ -15,9 +18,30 @@
         protected override void Seed(TeduShop.Data.TeduShopDbContext context)
         {
             //  This method will be called after migrating to the latest version.
+            var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new TeduShopDbContext()));
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
-            //  to avoid creating duplicate seed data.
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new TeduShopDbContext()));
+
+            var user = new ApplicationUser()
+            {
+                UserName = "loclx",
+                Email = "lexuanloc@outlook.com",
+                EmailConfirmed = true,
+                BirthDay = DateTime.Now,
+                FullName = "Le Xuan Loc"
+            };
+
+            manager.Create(user, "123@123a");
+
+            if (!roleManager.Roles.Any())
+            {
+                roleManager.Create(new IdentityRole { Name = "Admin" });
+                roleManager.Create(new IdentityRole { Name = "User" });
+            }
+
+            var adminUser = manager.FindByEmail("lexuanloc@outlook.com");
+
+            manager.AddToRoles(adminUser.Id, new string[] { "Admin", "User" });
         }
     }
 }
