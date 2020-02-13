3ec1ee1af2ae382b7b8ae6640d9f97749592143d
lexuanloc <lexuanloc@outlook.com>
2020-02-13 14:50:38 +0700

    Test Module và Controller của AngularJS

---
+++ TeduShop.Web/Scripts/spa/app.js
@@ -0,0 +1,8 @@
+﻿/// <reference path="../../node_modules/angular/angular.js" />
+var myApp = angular.module('myModule', []);
+myApp.controller("myController", myController);
+myApp.$inject = ['$scope'];
+
+function myController($scope) {
+    $scope.message = "This is a message from myController";
+}
