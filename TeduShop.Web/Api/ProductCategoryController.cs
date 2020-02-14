0f87810eb489577a2c91aaccfe80ed8f068899f3
lexuanloc <lexuanloc@outlook.com>
2020-02-14 16:55:55 +0700

    Custom directive để phân trang, áp dụng cho Danh mục sản phẩm

--- TeduShop.Web/Api/ProductCategoryController.cs
+++ TeduShop.Web/Api/ProductCategoryController.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
@@ -21,14 +23,39 @@ namespace TeduShop.Web.Api
             this._productCategoryService = productCategoryService;
         }
 
+        //[Route("getall")]
+        //public HttpResponseMessage GetAll(HttpRequestMessage request)
+        //{
+        //    return CreateHttpResponse(request, () =>
+        //    {
+        //        var model = _productCategoryService.GetAll();
+        //        var responseData = Mapper.Map<IEnumerable<ProductCategory>, IEnumerable<ProductCategoryViewModel>>(model);
+        //        var response = request.CreateResponse(HttpStatusCode.OK, responseData);
+        //        return response;
+        //    });
+        //}
+
         [Route("getall")]
-        public HttpResponseMessage GetAll(HttpRequestMessage request)
+        public HttpResponseMessage GetAll(HttpRequestMessage request, int page, int pageSize = 20)
         {
             return CreateHttpResponse(request, () =>
             {
+                int totalRow = 0;
                 var model = _productCategoryService.GetAll();
-                var responseData = Mapper.Map<IEnumerable<ProductCategory>, IEnumerable<ProductCategoryViewModel>>(model);
-                var response = request.CreateResponse(HttpStatusCode.OK, responseData);
+
+                totalRow = model.Count();
+                var query = model.OrderByDescending(x => x.CreatedDate).Skip(page * pageSize).Take(pageSize);
+
+                var responseData = Mapper.Map<IEnumerable<ProductCategory>, IEnumerable<ProductCategoryViewModel>>(query);
+
+                var paginationSet = new PaginationSet<ProductCategoryViewModel>()
+                {
+                    Items = responseData,
+                    Page = page,
+                    TotalCount = totalRow,
+                    TotalPages = (int)Math.Ceiling((double)totalRow / pageSize) // Làm tròn lên
+                };
+                var response = request.CreateResponse(HttpStatusCode.OK, paginationSet);
                 return response;
             });
         }
