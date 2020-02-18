5bbbfa06c1e7c3b6dfcf6a84b13b4317254832c1
lexuanloc <lexuanloc@outlook.com>
2020-02-18 11:16:15 +0700

    Sử dụng Angular validate cho form Add và Edit danh mục sản phẩm

--- TeduShop.Web/app/components/product_categories/productCategoryAddController.js
+++ TeduShop.Web/app/components/product_categories/productCategoryAddController.js
@@ -1,16 +1,20 @@
 ﻿(function (app) {
     app.controller('productCategoryAddController', productCategoryAddController);
 
-    productCategoryAddController.$inject = ['apiService', '$scope', 'notificationService', '$state'];
+    productCategoryAddController.$inject = ['apiService', '$scope', 'notificationService', '$state', 'commonService'];
 
-    function productCategoryAddController(apiService, $scope, notificationService, $state) {
+    function productCategoryAddController(apiService, $scope, notificationService, $state, commonService) {
         $scope.productCategory = {
             CreatedDate: new Date(),
-            Status: true,
-            Name: 'Danh mục 1'
+            Status: true
         }
 
         $scope.addProductCategory = addProductCategory;
+        $scope.getSeoTitle = getSeoTitle;
+
+        function getSeoTitle() {
+            $scope.productCategory.Alias = commonService.getSeoTitle($scope.productCategory.Name);
+        }
 
         function addProductCategory() {
             apiService.post('api/productcategory/create', $scope.productCategory, function (result) {
@@ -34,27 +38,3 @@
 
 
 })(angular.module('tedushop.product_categories'));
-
-//(function (app) {
-//    app.controller('productCategoryAddController', productCategoryAddController);
-
-//    productCategoryAddController.$inject = ['apiService', '$scope'];
-
-//    function productCategoryAddController(apiService, $scope) {
-//        $scope.productCategory = {
-//            CreatedDate: new Date()
-//        }
-//        $scope.productCategory = [];
-
-//        function loadParentCategory() {
-//            apiService.get('api/productcategory/getallparents', null, function (result) {
-//                $scope.parentCategories = result.data;
-//            }, function () {
-//                console.log('Cannot get list parent');
-//            });
-//        }
-
-//        loadParentCategory();
-//    }
-
-//})(angular.module('tedushop.product_categories'));
